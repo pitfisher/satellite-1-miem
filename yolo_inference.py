078459c3a683fa49edb9757d592d42d7039ca0f2
Petr Rybakov <prybakov@miem.hse.ru>
2023-06-13 12:33:12 +0300

    Added unfinished method to convert result to ods.ai YOLO format

--- yolo_inference.py
+++ yolo_inference.py
@@ -2,26 +2,48 @@ from ultralytics import YOLO
 import pandas as pd
 from os import listdir
 from os.path import isfile, join
+import sys
+from pathlib import Path
 
+TEST_IMAGES_PATH, SAVE_PATH = sys.argv[1:]
 
 model = YOLO('data/models/yolo-best-1206.pt')
 mypath = 'data/test_data/images'
-onlyfiles = [join(mypath, f) for f in listdir(mypath)]
-
+onlyfiles = [join(TEST_IMAGES_PATH, f) for f in listdir(TEST_IMAGES_PATH)]
 
 coordinates = pd.DataFrame({'image': [], 'output': [], 'score': []})
+
+def convert_detection_to_yolo(image_id, yolov8_result):
+    original_width, original_height = yolov8_result.orig_shape
+    x, y, w, h = yolov8_result.boxes.xywhn
+    result = {
+        'image_id': image_id,
+        'xc': round((box[1] + box[3])/2, 4), # center of bbox x coordinate
+        'yc': round((box[0] + box[2])/2, 4), # center of bbox y coordinate
+        'w': round(box[3] - box[1], 4), # width of bbox
+        'h': round(box[2] - box[0], 4), # height of bbox
+        'label': 0, # COCO class label
+        'score': round(score, 4) # class probability score
+    }
+    return result
 
 res = model.predict(
    source=onlyfiles,
    conf=0.25
 )
+# print("res: ", res)
 for result in res:
+    print("result.boxes: ", result.boxes)
+    image_id = Path(result.path).stem
     output = []
     scores = []
-    cords = result.boxes.xyxy
+    coords = result.boxes.xyxy
     sc = result.boxes.conf
-    for i, cor in enumerate(cords):
-        output.append(['person', int(cor[0]), int(cor[1]), int(cor[2]), int(cor[3])])
+    for i, coor in enumerate(coords):
+        print("image_id", image_id)
+        output.append(['person', int(coor[0]), int(coor[1]), int(coor[2]), int(coor[3])])
         scores.append(sc[i].item())
     
     coordinates.loc[len(coordinates)] = [result.path, output, scores]
+
+print(coordinates)
